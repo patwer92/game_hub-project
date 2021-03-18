e3e9facc0f0cdb0e1a951a3737ec43444505bfb1
Patrick <31260874+patwer92@users.noreply.github.com>
2021-03-18 23:51:43 +0100

    Added contact / newsletter form with javascript

--- js/script.js
+++ js/script.js
@@ -1,2 +1,61 @@
+const contactForm = document.querySelector("#contactForm");
+
+const contactName = document.querySelector("#contactName");
+const contactNameError = document.querySelector("#contactNameError");
+
+const contactEmail = document.querySelector("#contactEmail");
+const contactEmailError = document.querySelector("#contactEmailError");
+
+const subject = document.querySelector("#subject");
+const subjectError = document.querySelector("#subjectError");
 
 
+
+
+function validateForm() {
+
+    event.preventDefault();
+
+    if (checkLength(contactName.value, 0) === true) {
+        contactNameError.style.display = "none";
+    } else {
+        contactNameError.style.display = "block";
+        return false;
+    }
+
+    if(validateEmail(contactEmail.value) === true) {
+        contactEmailError.style.display = "none";
+    } else {
+        contactEmailError.style.display = "block";
+        return false;
+    }
+
+    if (checkLength(subject.value, 9) === true) {
+        subjectError.style.display = "none";
+    } else {
+        subjectError.style.display = "block";
+        return false;
+    }
+
+    alert("Your request has been successfully sent.");
+    return true;
+    
+
+}
+
+contactForm.addEventListener("submit", validateForm);
+
+function checkLength(value, len) {
+    if (value.trim().length > len) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateEmail(contactEmail) {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatches = regEx.test(contactEmail);
+    return patternMatches;
+
+}
